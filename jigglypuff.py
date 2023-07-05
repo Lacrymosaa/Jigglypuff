09c54d39d42b51f6a9bbeb84cceb2c0caa40d3e4
marian <yuzuvulpes@gmail.com>
2023-07-05 17:06:26 -0300

    Add files via upload

--- jigglypuff.py
+++ jigglypuff.py
@@ -1,7 +1,12 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import sys
+import json
 import random
 import webbrowser
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
 
 class Music:
     def __init__(self, name, link):
@@ -11,37 +16,92 @@ class Music:
     def __str__(self):
         return f"{self.name}: {self.link}"
 
-client_id = 'SEU_CLIENT_ID'
-client_secret = 'SEU_CLIENT_SECRET'
+class Jigglypuff(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-# Lê o conteúdo do arquivo id.txt e armazena na variável client_id
-with open("id.txt", "r") as file:
-    client_id = file.read().strip()
+    def initUI(self):
+        self.setWindowTitle('Jigglypuff')
+        self.setGeometry(300, 300, 250, 100)
+        self.setStyleSheet("background-color: #F0D2DB;")
 
-# Lê o conteúdo do arquivo secret.txt e armazena na variável client_secret
-with open("secret.txt", "r") as file:
-    client_secret = file.read().strip()
+        icon = QIcon("srcs/Jigglypuff.ico")
+        self.setWindowIcon(icon)
+        
+        font = QFont("Cascadia Mono", 12, QFont.Bold)
+        font.setLetterSpacing(QFont.AbsoluteSpacing, 1)
+        self.artist_label = QLabel('Artist Name:', self)
+        self.artist_label.setFont(font)
+        
+        self.artist_input = QLineEdit(self)
+        self.artist_input.setStyleSheet("border: 1px solid #000; background-color: #fff;")
 
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        self.random_button = QPushButton('Play!', self)
+        self.random_button.clicked.connect(self.play_random_music)
+        self.random_button.setStyleSheet("QPushButton {"
+                                          "background-color: #FFB6C1;"
+                                          "border: 1px solid black;"
+                                          "border-radius: 1px;"
+                                          "padding: 5px;"
+                                          "font-size: 14px;"
+                                          "letter-spacing: 1px;"
+                                          "}"
+                                          "QPushButton:hover {"
+                                          "background-color: #FFC0CB;"
+                                          "}"
+                                          "QPushButton:pressed {"
+                                          "background-color: #FFA07A;"
+                                          "}")
 
-# Busca as músicas da Taylor Swift
-artist_name = 'Taylor Swift'
-result = sp.search(artist_name, type='artist')
-artist_uri = result['artists']['items'][0]['uri']
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.artist_label)
+        vbox.addWidget(self.artist_input)
+        vbox.addWidget(self.random_button)
 
-results = sp.artist_albums(artist_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = sp.next(results)
-    albums.extend(results['items'])
+        self.setLayout(vbox)
+        self.show()
 
-tracks = []
-for album in albums:
-    results = sp.album_tracks(album['uri'])
-    tracks.extend(results['items'])
+    def play_random_music(self):
+        artist_name = self.artist_input.text()
 
-# Escolhe uma música aleatória
-random_track = random.choice(tracks)
-music = Music(random_track['name'], random_track['external_urls']['spotify'])
-webbrowser.open(music.link)
+        client_id = 'SEU_CLIENT_ID'
+        client_secret = 'SEU_CLIENT_SECRET'
+
+        # Lê as credenciais do arquivo JSON
+        with open("credentials.json", "r") as file:
+            credentials = json.load(file)
+
+        client_id = credentials["client_id"]
+        print(client_id)
+        client_secret = credentials["client_secret"]
+        print(client_secret)
+
+        client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+        result = sp.search(artist_name, type='artist')
+        artist_uri = result['artists']['items'][0]['uri']
+
+        results = sp.artist_albums(artist_uri, album_type='album')
+        albums = results['items']
+        while results['next']:
+            results = sp.next(results)
+            albums.extend(results['items'])
+
+        tracks = []
+        for album in albums:
+            results = sp.album_tracks(album['uri'])
+            tracks.extend(results['items'])
+
+        if tracks:
+            random_track = random.choice(tracks)
+            music = Music(random_track['name'], random_track['external_urls']['spotify'])
+            webbrowser.open(music.link)
+        else:
+            print(f"No tracks found for the artist: {artist_name}")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    music_app = Jigglypuff()
+    sys.exit(app.exec_())
